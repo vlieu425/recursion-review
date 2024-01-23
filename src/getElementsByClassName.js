6799e4db6219d7a2c604c00baea5d6eb537780bc
James Clover <jamesmclover93@gmail.com>
2024-01-22 20:10:01 -0500

    completed stringifyJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,24 @@
 //   return document.getElementsByClassName(className);
 // };
 
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, rootNode
 ) {
   // your code here
+  var root = rootNode || document.body;
+  var result = [];
+
+  if (root.classList && root.classList.contains(className)) {
+    result.push(root);
+  }
+
+  if (root.hasChildNodes) {
+    var children = root.childNodes;
+    for (var i = 0; i < children.length; i ++) {
+      var child = children[i];
+      result = result.concat(getElementsByClassName(className, child))
+    }
+  }
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "number" || typeof obj === "boolean") {
+    return String(obj);
+    // AssertionError: expected '"9"' to equal '9'
+  }
+  if (typeof obj === "string") {
+    return '"' + obj + '"';
+  }
+
+  if (obj === null) {
+    return 'null'
+  }
+
+  if (Array.isArray(obj)) {
+    var res = [];
+    for (var i = 0; i < obj.length; i++) {
+      var item = obj[i];
+      res.push(stringifyJSON(item));
+    }
+    return "[" + res.join(',') + "]";
+  }
+
+  if (typeof obj === "object") {
+    var res = [];
+    for (var key in obj) {
+      if (typeof obj[key] !== "function" && obj[key] !== undefined) {
+      var stringKey = stringifyJSON(key);
+      var stringValue = stringifyJSON(obj[key]);
+      res.push(stringKey + ":" + stringValue);
+    }
+  }
+    return "{" + res.join(',') + "}";
+}
 };
+
+// console.log(stringifyJSON({"test": function(){}, "test2": undefined}));
+// console.log(stringifyJSON([]));
+// console.log("REAL:::", JSON.stringify([]));
+// console.log(JSON.stringify(({"test": function(){}, "test2": undefined})));
